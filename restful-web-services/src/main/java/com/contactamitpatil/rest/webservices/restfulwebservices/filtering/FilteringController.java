afc8777a47261e8bd1f35f79bd1090511c8c9758
Amit Patil <contact.amit.patil@gmail.com>
2020-06-07 22:16:23 +0530

    Second Commit

---
+++ restful-web-services/src/main/java/com/contactamitpatil/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -0,0 +1,49 @@
+package com.contactamitpatil.rest.webservices.restfulwebservices.filtering;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+
+@RestController
+public class FilteringController {
+
+	@GetMapping("/filtering")
+	public MappingJacksonValue retrieveSomeBean() {
+		SomeBean someBean = new SomeBean("values1", "value2", "value3");
+		
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+				.filterOutAllExcept("field1", "field2");
+		
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+		
+		MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+		mapping.setFilters(filters);
+		
+		 
+		return mapping;
+	}
+	
+	@GetMapping("/filtering-list")
+	public MappingJacksonValue retrieveListOfSomeBean() {
+		List<SomeBean> list = Arrays.asList(new SomeBean("values1", "value2", "value3"),
+				new SomeBean("values11", "value21", "value31"));
+		
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+				.filterOutAllExcept("field3");
+		
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+		
+		MappingJacksonValue mapping = new MappingJacksonValue(list);
+		mapping.setFilters(filters);
+		
+		 
+		return mapping;
+	}
+}
